a5b72f7ee8a82d573b8630b8efd67be4c1cf9635
ankita-vc <140981444+ankita-vc@users.noreply.github.com>
2023-08-06 05:55:51 +0530

    Add files via upload

---
+++ Program201.c
@@ -0,0 +1,56 @@
+/*
+  Accept string from user and check whether it is pallindrome or not
+*/ 
+
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckPallindrome(char *str)
+{
+  char *start= NULL;
+  char *end= NULL;
+  bool bFlag= true;
+
+  start= str;
+  end= str;
+
+  while(*end != '\0')
+  {
+    end++;
+  }
+  end--;
+
+  while(start<end)
+  {
+    if(*start != *end)
+    {
+      bFlag= false;
+      break;
+    }
+    start++;
+    end--;
+  }
+  return bFlag;
+}
+
+int main()
+{
+  char Arr[20];
+  bool bRet= false;
+
+  printf("Enter the string:\n");
+  scanf("%[^'\n']s", Arr);
+
+  bRet= CheckPallindrome(Arr);
+
+  if(bRet == true)
+  {
+    printf("String is pallindrome\n");
+  }
+  else
+  {
+    printf("String is not pallindrome\n");
+  }
+
+  return 0;
+}
---
+++ Program202.c
@@ -0,0 +1,64 @@
+/*
+  Accept string from user and check whether it is pallindrome or not
+*/ 
+
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckPallindrome(char *str)
+{
+  char *start= NULL;
+  char *end= NULL;
+
+  start= str;
+  end= str;
+
+  while(*end != '\0')
+  {
+    end++;
+  }
+  end--;
+
+  while(start<end)
+  {
+    if(*start != *end)
+    {
+      break;
+    }
+    
+    start++;
+    end--;
+  }
+
+  if(start < end)
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+  
+}
+
+int main()
+{
+  char Arr[20];
+  bool bRet= false;
+
+  printf("Enter the string:\n");
+  scanf("%[^'\n']s", Arr);
+
+  bRet= CheckPallindrome(Arr);
+
+  if(bRet == true)
+  {
+    printf("String is pallindrome\n");
+  }
+  else
+  {
+    printf("String is not pallindrome\n");
+  }
+
+  return 0;
+}
